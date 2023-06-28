856eaea1eaf1937f8edd9358ba86c24e6f88ad3e
flighty <vrtxrry@pm.me>
2023-06-29 02:33:18 +0300

    Add bottom margin to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
 	return(
 
-		<footer className="fly-footer fly-text-center">
+		<footer className="fly-footer fly-text-center fly-margin-bottom">
 			<span>&copy; {year} flighty</span>
 		</footer>
 	);
