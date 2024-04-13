b2da4dd9e75fe2385a53e2b6d747b949d0b0d6d9
Oğuzhan <37447279+ogoregen@users.noreply.github.com>
2024-04-13 23:46:59 +0300

    Update

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -11,18 +11,13 @@ const Layout = ({title, subtitle, children}) => {
 		var subtitleElement = <span className="fly-text-muted">{subtitle}</span>;
 	}
 
-	if(title){
-
-		var breadcrumbs = <span> / {title}</span>
-	}
-
 	return(
 
 		<div className="fly-container" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between',  minHeight: '100vh'}}>
 			
 			<div style={{padding: '1rem'}}>
 				<main className="fly-width-2-3 fly-margin-horizontal-auto">
-					<div class="fly-margin-bottom"><Link to="/">ogoregen.com</Link>{breadcrumbs}</div>
+					<div class="fly-margin-bottom"><Link to="/">ogoregen.com</Link></div>
 					<div className="fly-text-center">
 						<h1 className="fly-margin-small-bottom">{title}</h1>
 						{subtitleElement}
